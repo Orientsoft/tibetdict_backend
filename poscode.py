80e6010c65cba1c4c26f1a2a9b726f7dc93f84f4
nickery <xuyilin@orientsoft.cn>
2021-03-08 10:43:33 +0800

    update id

--- poscode.py
+++ poscode.py
@@ -1,14 +1,14 @@
 data = [
-    {'en-us': 'n.', 'zh-cn': '名', 'bo-cn': 'མིང'},
-    {'en-us': 'v.', 'zh-cn': '动', 'bo-cn': 'བྱ'},
-    {'en-us': 'adj.', 'zh-cn': '形', 'bo-cn': 'རྒྱན'},
-    {'en-us': 'adv.', 'zh-cn': '副', 'bo-cn': 'བསྣན'},
-    {'en-us': 'pron.', 'zh-cn': '代', 'bo-cn': 'ཚབ'},
-    {'en-us': 'clf.', 'zh-cn': '量', 'bo-cn': 'ཚད'},
-    {'en-us': 'num.', 'zh-cn': '数', 'bo-cn': 'གྲངས'},
-    {'en-us': 'prep.', 'zh-cn': '介', 'bo-cn': 'སྦྱོར། '},
-    {'en-us': 'conj.', 'zh-cn': '连', 'bo-cn': 'སྦྲེལ། '},
-    {'en-us': 'aux.', 'zh-cn': '助', 'bo-cn': 'གྲོགས། '},
-    {'en-us': 'int.', 'zh-cn': '叹', 'bo-cn': 'འབོད། '},
-    {'en-us': 'ono.', 'zh-cn': '拟声', 'bo-cn': 'སྒྲ་མཚོན། '}
+    {'id': '0', 'en-us': 'n.', 'zh-cn': '名', 'bo-cn': 'མིང'},
+    {'id': '1', 'en-us': 'v.', 'zh-cn': '动', 'bo-cn': 'བྱ'},
+    {'id': '2', 'en-us': 'adj.', 'zh-cn': '形', 'bo-cn': 'རྒྱན'},
+    {'id': '3', 'en-us': 'adv.', 'zh-cn': '副', 'bo-cn': 'བསྣན'},
+    {'id': '4', 'en-us': 'pron.', 'zh-cn': '代', 'bo-cn': 'ཚབ'},
+    {'id': '5', 'en-us': 'clf.', 'zh-cn': '量', 'bo-cn': 'ཚད'},
+    {'id': '6', 'en-us': 'num.', 'zh-cn': '数', 'bo-cn': 'གྲངས'},
+    {'id': '7', 'en-us': 'prep.', 'zh-cn': '介', 'bo-cn': 'སྦྱོར། '},
+    {'id': '8', 'en-us': 'conj.', 'zh-cn': '连', 'bo-cn': 'སྦྲེལ། '},
+    {'id': '9', 'en-us': 'aux.', 'zh-cn': '助', 'bo-cn': 'གྲོགས། '},
+    {'id': '10', 'en-us': 'int.', 'zh-cn': '叹', 'bo-cn': 'འབོད། '},
+    {'id': '11', 'en-us': 'ono.', 'zh-cn': '拟声', 'bo-cn': 'སྒྲ་མཚོན། '}
 ]
