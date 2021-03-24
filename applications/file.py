ec0a1878af7a9b84a09dc4572c65b12abdd432fd
bilin.wang <wangbilin@orientsoft.cn>
2021-03-24 15:19:00 +0800

    file content

--- applications/file.py
+++ applications/file.py
@@ -30,6 +30,7 @@ from datetime import datetime
 from common.cache import get_cache, set_cache, del_cache
 import sys
 from common.search import query_es
+from common.worker import celery_app
 
 router = APIRouter()
 _platform = platform.system().lower()
@@ -38,80 +39,6 @@ _platform = platform.system().lower()
 class UploadError(Exception):
     def __init__(self):
         self.msg = '40017'
-
-
-#
-# @router.post('/file', tags=['file'], name='文档上传')
-# async def upload_file(file: UploadFile = File(...), user: User = Depends(get_current_user_authorizer()),
-#                       db: AsyncIOMotorClient = Depends(get_database)):
-#     attr = file.filename.rsplit('.')[-1]
-#     if attr not in ['txt', 'docx', 'doc']:
-#         raise HTTPException(status_code=400, detail='100141')
-#     data = FileCreateModel(
-#         user_id=user.id,
-#         file_name=file.filename,
-#     )
-#     '''
-#     1.txt 本地存储，
-#     2.docx python-docx转换
-#     3.doc 不同平台不同方法，windows暂不支持
-#     '''
-#     origin_content = None
-#     if attr == 'txt':
-#         origin_content = file.file.read().decode('utf-8')
-#     elif attr in ['docx', 'doc']:
-#         # 临时目录 存储到本地
-#         if not os.path.exists('temp'):
-#             os.mkdir('temp')
-#         origin_temp_file_name = f"temp/{data.id}.{attr}"
-#         with open(origin_temp_file_name, "wb") as buffer:
-#             shutil.copyfileobj(file.file, buffer)
-#
-#         if attr == 'docx':
-#             tmp = []
-#             doc_file = docx.Document(origin_temp_file_name)
-#             for para in doc_file.paragraphs:
-#                 tmp.append(para.text)
-#             origin_content = '\n'.join(tmp)
-#         elif attr == 'doc':
-#             saveas_txt_file_name = f"temp/{data.id}.txt"
-#             if _platform == 'linux':
-#                 cmd = f"catdoc {origin_temp_file_name} > {saveas_txt_file_name}"
-#                 os.system(cmd)
-#             elif _platform == 'darwin':
-#                 cmd = f"textutil -convert txt {origin_temp_file_name} -output {saveas_txt_file_name}"
-#                 os.system(cmd)
-#             else:
-#                 raise HTTPException(status_code=400, detail='100141')
-#             with open(saveas_txt_file_name, 'r') as f:
-#                 origin_content = f.read()
-#             os.remove(saveas_txt_file_name)
-#         # 删除临时文件
-#         os.remove(origin_temp_file_name)
-#
-#     m = MinioUploadPrivate()
-#
-#     # 原始文件
-#     data.origin = f'origin/{user.id}/{data.id}.txt'
-#     data.parsed = f'parsed/{user.id}/{data.id}.txt'
-#     # 上传原始文件
-#     m.commit(origin_content.encode('utf-8'), data.origin)
-#     # 已分词
-#     if len(origin_content[:500].split(' ')) <= 5:
-#         logger.info('未分词文档，自动分词中')
-#         parsed_content = tokenize_words(origin_content)
-#         # logger.info(parsed_content)
-#         data.is_check = True
-#     else:
-#         parsed_content = origin_content
-#     parsed_content = re.sub(r"།(\s*)།", r"།།\r\n", parsed_content)
-#     # 提交分词结果
-#     m.commit(parsed_content.encode('utf-8'), data.parsed)
-#     # 文件指纹
-#     data.p_hash = contenttomd5(parsed_content.encode('utf-8'))
-#     data.o_hash = contenttomd5(origin_content.encode('utf-8'))
-#     await create_file(db, data)
-#     return {'id': data.id}
 
 
 @router.get('/my/file', tags=['file'], name='我的文件')
@@ -132,7 +59,7 @@ async def get_my_file(user_id: str = None, search: str = None, is_check: bool = 
 
 
 @router.get('/file/content', tags=['file'], name='某一文件内容')
-async def get_file_content(file_id: str,
+async def get_file_content(file_id: str, is_origin: bool = False,
                            user: User = Depends(get_current_user_authorizer()),
                            db: AsyncIOMotorClient = Depends(get_database)):
     returnObj = {
@@ -146,7 +73,7 @@ async def get_file_content(file_id: str,
     if db_file.user_id != user.id and db_file.user_id != SHARE_USER_ID:
         raise HTTPException(HTTP_400_BAD_REQUEST, '40005')
     m = MinioUploadPrivate()
-    content = m.get_object(db_file.parsed or db_file.origin)
+    content = m.get_object(db_file.parsed or db_file.origin if not is_origin else db_file.origin)
     temp_content = tokenize_sentence(content.decode('utf-8'))
     seq = 1
     for r in temp_content:
@@ -174,6 +101,8 @@ async def patch_file(file_id: str = Body(...), content: str = Body(None), is_che
         raise HTTPException(HTTP_400_BAD_REQUEST, '40005')
     update_obj = {}
     if is_check is not None:
+        if is_check and content is None and db_file.parsed is None:
+            raise HTTPException(HTTP_400_BAD_REQUEST, '40018')
         update_obj['is_check'] = is_check
     if content is not None:
         m = MinioUploadPrivate()
@@ -440,19 +369,20 @@ async def search_file(search: str = Body(...), origin: OriginEnum = Body(...), p
     return returnObj
 
 
-# @router.get('/file/tokenize', tags=['file'], name='文件自动分词')
-# async def tokenize(file_id: str,
-#                    user: User = Depends(get_current_user_authorizer()),
-#                    db: AsyncIOMotorClient = Depends(get_database)):
-#     db_file = await get_file(db, {'id': file_id})
-#     if not db_file:
-#         raise HTTPException(HTTP_400_BAD_REQUEST, '40011')
-#     if db_file.user_id != user.id and 0 not in user.role:
-#         raise HTTPException(HTTP_400_BAD_REQUEST, '40005')
-#     m = MinioUploadPrivate()
-#     content = m.get_object(db_file.parsed)
-#     parsed_content = tokenize_words(content.decode('utf-8'))
-#     return {'data': parsed_content, 'file_name': db_file.file_name}
+@router.get('/file/tokenize', tags=['file'], name='文件自动分词')
+async def tokenize(file_id: str, is_async: bool = False,
+                   user: User = Depends(get_current_user_authorizer()),
+                   db: AsyncIOMotorClient = Depends(get_database)):
+    db_file = await get_file(db, {'id': file_id})
+    if not db_file:
+        raise HTTPException(HTTP_400_BAD_REQUEST, '40011')
+    if db_file.user_id != user.id and 0 not in user.role:
+        raise HTTPException(HTTP_400_BAD_REQUEST, '40005')
+    resp = celery_app.send_task('worker:origin_tokenize', args=[file_id], queue='tibetan',
+                                routing_key='tibetan')
+    if is_async is False:
+        resp = resp.get(timeout=300)
+    return {'msg': '2001'}
 
 
 @router.post('/file/search', tags=['file'], name='搜索某一文件内容')
